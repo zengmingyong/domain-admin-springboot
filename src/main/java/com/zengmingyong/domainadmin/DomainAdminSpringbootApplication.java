dceee0d891f609fb9d78201e06ce238d601d8439
zengmingyong <42780443+zengmingyong@users.noreply.github.com>
2025-12-06 15:16:43 +0800

    Add Spring Boot 3 main entry point

---
+++ src/main/java/com/zengmingyong/domainadmin/DomainAdminSpringbootApplication.java
@@ -0,0 +1,11 @@
+package com.zengmingyong.domainadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainAdminSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DomainAdminSpringbootApplication.class, args);
+    }
+}
